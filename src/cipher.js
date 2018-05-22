0847425b29062fd0249d649df68c1ff24e74c9f1
andrealemonroy <andreale17@icloud.com>
2018-05-22 12:25:54 -0500

    básico función solo funciona con letras

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,25 @@
-window.cipher = {
-  // ... 
-};
+function encrypt() {
+  textOne = document.getElementById("textOne").value;
+  offset = document.getElementById("offset").value;
+  textTwo = document.getElementById("textTwo");
+  var output = "";
+  offset = parseInt(offset);
+  for (let i = 0; i < textOne.length; i++) {
+    numberLetter=(textOne.charCodeAt(i)+offset)
+    if (numberLetter<90 && numberLetter>65 || numberLetter<122 && numberLetter>97) {
+    output += String.fromCharCode(numberLetter);
+    textTwo.value=output;
+    }
+    else alert("funciona!");
+  }
+}
+function decrypt() {
+  textTwo = document.getElementById("textTwo").value;
+  offset = document.getElementById("offset").value;
+  var output = "";
+  offset = parseInt(offset);
+  for (var i = 0; i < textTwo.length; i++) {
+    output += String.fromCharCode(textTwo.charCodeAt(i) - offset);
+  }
+  alert(output);
+}
