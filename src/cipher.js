3fb3ca22510f119c74e7aca1c0ba148c044c3901
andrealemonroy <andreale17@icloud.com>
2018-05-24 12:27:28 -0500

    funciona sin estilo

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,85 @@
+window.addEventListener("load", function() {
+  document.getElementById("buttonEncrypt").addEventListener("click", encrypt, true);
+  document.getElementById("buttonDecrypt").addEventListener("click", decrypt, true);
+  document.getElementById("clearOne").addEventListener("click", clearOne, true);
+  document.getElementById("clearTwo").addEventListener("click", clearTwo, true);
+});
+function clearOne() {
+    document.getElementById("textOne").value="";
+} 
+function clearTwo() {
+    document.getElementById("textTwo").value="";
+} 
+ function onlyText(){
+    var out = '';
+    var string = document.getElementById("textOne").value;
+    var filtro = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (var i=0; i<string.length; i++)
+       if (filtro.includes(string.charAt(i))) 
+     out += string.charAt(i);
+   document.getElementById("textOne").value=out;
+}
+ function onlyText2(){
+    var out = '';
+    var string = document.getElementById("textTwo").value;
+    var filtro = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (var i=0; i<string.length; i++)
+       if (filtro.includes(string.charAt(i))) 
+     out += string.charAt(i);
+   document.getElementById("textTwo").value=out;
+}
 function encrypt() {
-  textOne = document.getElementById("textOne").value;
-  offset = document.getElementById("offset").value;
-  textTwo = document.getElementById("textTwo");
-  var output = "";
-  offset = parseInt(offset);
-  for (let i = 0; i < textOne.length; i++) {
-    numberLetter=(textOne.charCodeAt(i)+offset)
-    if (numberLetter<90 && numberLetter>65 || numberLetter<122 && numberLetter>97) {
-    output += String.fromCharCode(numberLetter);
-    textTwo.value=output;
+    if (document.getElementById("offset").value < 1) document.getElementById("offset").value = 1;
+    onlyText();
+    texto=document.getElementById("textOne").value;
+    offset=document.getElementById("offset").value;
+    var output="";
+    var caracter="";
+    offset = parseInt(offset);
+    for (var i=0; i<texto.length;i++){
+      numberLetter=texto.charCodeAt(i);
+      if(numberLetter==32){
+          output += " ";
+      }
+      else {
+          if(numberLetter<=90){
+            output += String.fromCharCode((numberLetter-65+offset)%26+65);
+          }
+          else{
+            output += String.fromCharCode((numberLetter-97+offset)%26+97);
+          }
+      } 
     }
-    else alert("funciona!");
-  }
-}
+    document.getElementById("textTwo").value = output; 
+} 
 function decrypt() {
-  textTwo = document.getElementById("textTwo").value;
-  offset = document.getElementById("offset").value;
-  var output = "";
-  offset = parseInt(offset);
-  for (var i = 0; i < textTwo.length; i++) {
-    output += String.fromCharCode(textTwo.charCodeAt(i) - offset);
-  }
-  alert(output);
-}
+    onlyText2();
+    texto=document.getElementById("textTwo").value;
+    offset=document.getElementById("offset").value;
+    var output="";
+    var caracter="";
+    offset = parseInt(offset);
+    for (var i=0; i<texto.length; i++){
+      numberLetter=texto.charCodeAt(i);
+      if (numberLetter == 32){
+        output += " ";
+      }
+      else{
+          if (numberLetter<=90) {
+                if ((texto.charCodeAt(i)-offset) < 65){
+                    difference=(65-(numberLetter-offset%26));
+                    output += String.fromCharCode(91-difference);
+                }
+                else output += String.fromCharCode(texto.charCodeAt(i)-offset%26);
+          }
+          else{
+                if ((texto.charCodeAt(i)-offset) < 97){
+                    difference=(97-(numberLetter-offset%26));
+                    output += String.fromCharCode(123-difference);
+                }
+                else output += String.fromCharCode(texto.charCodeAt(i)-offset%26);
+          }    
+      }
+    }
+        document.getElementById("textOne").value = output; 
+} 
