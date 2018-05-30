192736a51b6aff3174d64ea317f0005e4692eabe
andrealemonroy <andreale17@icloud.com>
2018-05-30 11:10:14 -0500

    funciona ecma s6

--- src/cipher.js
+++ src/cipher.js
@@ -1,80 +1,82 @@
 
-// window.cipher = {
-//     encrypt : encrypt,
-//     decrypt : decrypt,
-// };
-// function onlyText(){
-//     var out = '';
-//     var string = document.getElementById("textOne").value;
-//     var filtro = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//     for (var i=0; i<string.length; i++)
-//        if (filtro.includes(string.charAt(i))) 
-//      out += string.charAt(i);
-//    document.getElementById("textOne").value=out;
-//   }
+window.cipher = {
+  //encode : encode,
+  //decrypt : decrypt,
 
-//   function onlyText2(){
-//     var out = '';
-//     var string = document.getElementById("textTwo").value;
-//     var filtro = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//     for (var i=0; i<string.length; i++)
-//        if (filtro.includes(string.charAt(i))) 
-//      out += string.charAt(i);
-//    document.getElementById("textTwo").value=out;
-//   }
 
-// function encrypt() {
-//   onlyText();
-//   texto=document.getElementById("textOne").value;
-//   offset=document.getElementById("offset").value;
-//   var output="";
-//   offset = parseInt(offset);
-//   for (var i=0; i<texto.length;i++){
-//     numberLetter=texto.charCodeAt(i);
-//     if(numberLetter==32){
-//         output += " ";
-//     }
-//     else {
-//         if(numberLetter<=90){
-//           output += String.fromCharCode((numberLetter-65+offset)%26+65);
-//         }
-//         else{
-//           output += String.fromCharCode((numberLetter-97+offset)%26+97);
-//         }
-//     } 
-//   }
-//   document.getElementById("textTwo").value = output; 
-// } 
-// function decrypt() {
-//   onlyText2();
-//   texto=document.getElementById("textTwo").value;
-//   offset=document.getElementById("offset").value;
-//   var output="";
-//   for (var i=0; i<texto.length; i++){
-//     numberLetter=texto.charCodeAt(i);
-//     if (numberLetter == 32){
-//       output += " ";
-//     }
-//     else{
-//         if (numberLetter<=90 && numberLetter>=65) {
-//               if (texto.charCodeAt(i)-offset < 65 && texto.charCodeAt(i)-offset%26 < 65 ) {
-//                   difference=(65-(numberLetter-offset%26));
-//                   output += String.fromCharCode(91-difference);
-//               }
-//               else{ 
-//                   output += String.fromCharCode(texto.charCodeAt(i)-offset%26);
-//               }
-//         }
-//         else{
-//               if (numberLetter>= 97 && numberLetter <= 122 ){
-//                   if (texto.charCodeAt(i)-offset< 97 && texto.charCodeAt(i)-offset%26 < 97){
-//                   var difference=(97-(numberLetter-(offset)%26));
-//                   output += String.fromCharCode(123-difference);
-//               }
-//                   else output += String.fromCharCode(texto.charCodeAt(i)-offset%26);
-//         }    
-//     }
-//   }
-// }
-//       document.getElementById("textOne").value = output; 
-// } 
+  encode(offset, text) {
+    // onlyText();
+
+    // let offset=document.getElementById("offset").value;
+    // if (text.length==0){
+    //   alert("Ingrese text a procesar, gracias");
+    //   text.focus();
+    // }
+    // else if (offset<=0){
+    //   alert("Ingrese offset para procesar, gracias");
+    //   offset.focus();
+    // }
+    // else {
+    let output = "";
+    offset = parseInt(offset);
+    for (let i = 0; i < text.length; i++) {
+      //alert(text.length);
+      let numberLetter = text.charCodeAt(i);
+      //alert(numberLetter);
+      //if (specialText.includes(String.fromCharCode(numberLetter))) {
+      //output += String.fromCharCode(numberLetter);
+      //}
+      //else {
+      if (numberLetter <= 90) {
+        output += String.fromCharCode((numberLetter - 65 + offset) % 26 + 65);
+      }
+      else {
+        output += String.fromCharCode((numberLetter - 97 + offset) % 26 + 97);
+      }
+    }
+    //}
+    return output;
+  },
+
+  decode(offset, text) {
+    // onlyText2();
+
+    // let offset=document.getElementById("offset").value;
+    // if (text.length==0){
+    //   alert("Ingrese text a procesar, gracias");
+    //   text.focus();
+    // }
+    // else if (offset<=0){
+    //   alert("Ingrese offset para procesar, gracias");
+    //   offset.focus();
+    // }
+    // else {
+    let output = "";
+    for (i = 0; i < text.length; i++) {
+      numberLetter = text.charCodeAt(i);
+      // if (specialText.includes(String.fromCharCode(numberLetter))) {
+      //   output += String.fromCharCode(numberLetter);
+      // }
+      // else {
+      if (numberLetter <= 90 && numberLetter >= 65) {
+        if (text.charCodeAt(i) - offset < 65 && text.charCodeAt(i) - offset % 26 < 65) {
+          difference = (65 - (numberLetter - offset % 26));
+          output += String.fromCharCode(91 - difference);
+        }
+        else output += String.fromCharCode(text.charCodeAt(i) - offset % 26);
+
+      }
+      else {
+        if (numberLetter >= 97 && numberLetter <= 122) {
+          if (text.charCodeAt(i) - offset < 97 && text.charCodeAt(i) - offset % 26 < 97) {
+            let difference = (97 - (numberLetter - (offset) % 26));
+            output += String.fromCharCode(123 - difference);
+          }
+          else output += String.fromCharCode(text.charCodeAt(i) - offset % 26);
+        }
+      }
+    }
+    return output;
+  }
+  //} 
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-// document.getElementById("buttonEncrypt").addEventListener("click", encrypt);
+const normalText = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const specialText = " 0123456789¿?!¡,;:'.áéíóúñÑ";
+const filtro = normalText + specialText;
+let offset;
 
-// document.getElementById("buttonDecrypt").addEventListener("click", decrypt);
+document.getElementById("buttonStart").addEventListener("click", () => {
+    text = document.getElementById("textOne").value;
+    let offset = document.getElementById("offset").value;
+    const opcion = document.getElementById("list");
+    let value = opcion.options[opcion.selectedIndex].value;
+    if (value == "value1") {
+        document.getElementById("textTwo").innerHTML = cipher.decode(offset, text);
+        let result = document.getElementById("result")
+        result.innerHTML = "Texto desencriptado";
+    }
+    else {
+        let result = document.getElementById("result")
+        result.innerHTML = "Texto encriptado";
+        document.getElementById("textTwo").value = cipher.encode(offset, text);
+    }
+})
+document.getElementById("clear").addEventListener("click", () => {
+    document.getElementById("textOne").value = "";
+    document.getElementById("textTwo").value = "";
+})
 
-// document.getElementById("clearOne").addEventListener("click", function(){
-//     document.getElementById("textOne").value="";
-// });
+document.getElementById("textOne").addEventListener("keyup", () => {
+    let out = '';
+    let string = document.getElementById("textOne").value;
+    for (let i = 0; i < string.length; i++)
+        if (filtro.includes(string.charAt(i)))
+            out += string.charAt(i);
+    document.getElementById("textOne").value = out;
 
-// document.getElementById("clearTwo").addEventListener("click", function(){
-//     document.getElementById("textTwo").value="";
-// });
+})
 
+document.getElementById("textTwo").addEventListener("keyup", () => {
+    let out = '';
+    let string = document.getElementById("textTwo").value;
+    for (i = 0; i < string.length; i++)
+        if (filtro.includes(string.charAt(i)))
+            out += string.charAt(i);
+    document.getElementById("textTwo").value = out;
+
+})
