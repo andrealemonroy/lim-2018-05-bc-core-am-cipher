2e55d04682509998eeb1d8dd13e026e3b80f61a0
andrealemonroy <andreale17@icloud.com>
2018-05-31 11:35:53 -0500

    paso test y funciona!

--- src/cipher.js
+++ src/cipher.js
@@ -1,82 +1,39 @@
 
 window.cipher = {
-  //encode : encode,
-  //decrypt : decrypt,
+    encode(offset, text) {
+        let output = '';
+        for (let i = 0; i < text.length; i++) {
+            let numberLetter = text.charCodeAt(i);
+            if ( numberLetter >= 65 && numberLetter <= 90) {
+                output += String.fromCharCode((numberLetter - 65 + offset) % 26 + 65);
+            }
+            else if (numberLetter >= 97 && numberLetter <= 122) {
+                output += String.fromCharCode((numberLetter - 97 + offset) % 26 + 97);
+            }
+            }
+        return output;
+    },
 
+    decode(offset, text) {
+        let output = '';
+        for (let i = 0; i < text.length; i++) {
+            let numberLetter = text.charCodeAt(i);
+            if (numberLetter <= 90 && numberLetter >= 65) {
+                if (text.charCodeAt(i) - offset < 65 && text.charCodeAt(i) - offset % 26 < 65) {
+                    let difference = (65 - (numberLetter - offset % 26));
+                    output += String.fromCharCode(91 - difference);
+                }
+                else output += String.fromCharCode(numberLetter - offset % 26);
 
-  encode(offset, text) {
-    // onlyText();
-
-    // let offset=document.getElementById("offset").value;
-    // if (text.length==0){
-    //   alert("Ingrese text a procesar, gracias");
-    //   text.focus();
-    // }
-    // else if (offset<=0){
-    //   alert("Ingrese offset para procesar, gracias");
-    //   offset.focus();
-    // }
-    // else {
-    let output = "";
-    offset = parseInt(offset);
-    for (let i = 0; i < text.length; i++) {
-      //alert(text.length);
-      let numberLetter = text.charCodeAt(i);
-      //alert(numberLetter);
-      //if (specialText.includes(String.fromCharCode(numberLetter))) {
-      //output += String.fromCharCode(numberLetter);
-      //}
-      //else {
-      if (numberLetter <= 90) {
-        output += String.fromCharCode((numberLetter - 65 + offset) % 26 + 65);
-      }
-      else {
-        output += String.fromCharCode((numberLetter - 97 + offset) % 26 + 97);
-      }
-    }
-    //}
-    return output;
-  },
-
-  decode(offset, text) {
-    // onlyText2();
-
-    // let offset=document.getElementById("offset").value;
-    // if (text.length==0){
-    //   alert("Ingrese text a procesar, gracias");
-    //   text.focus();
-    // }
-    // else if (offset<=0){
-    //   alert("Ingrese offset para procesar, gracias");
-    //   offset.focus();
-    // }
-    // else {
-    let output = "";
-    for (i = 0; i < text.length; i++) {
-      numberLetter = text.charCodeAt(i);
-      // if (specialText.includes(String.fromCharCode(numberLetter))) {
-      //   output += String.fromCharCode(numberLetter);
-      // }
-      // else {
-      if (numberLetter <= 90 && numberLetter >= 65) {
-        if (text.charCodeAt(i) - offset < 65 && text.charCodeAt(i) - offset % 26 < 65) {
-          difference = (65 - (numberLetter - offset % 26));
-          output += String.fromCharCode(91 - difference);
-        }
-        else output += String.fromCharCode(text.charCodeAt(i) - offset % 26);
-
-      }
-      else {
-        if (numberLetter >= 97 && numberLetter <= 122) {
-          if (text.charCodeAt(i) - offset < 97 && text.charCodeAt(i) - offset % 26 < 97) {
-            let difference = (97 - (numberLetter - (offset) % 26));
-            output += String.fromCharCode(123 - difference);
-          }
-          else output += String.fromCharCode(text.charCodeAt(i) - offset % 26);
-        }
-      }
-    }
-    return output;
-  }
-  //} 
+            }
+            else if (numberLetter >= 97 && numberLetter <= 122) {
+                if ( numberLetter - offset % 26 < 97) {
+                    let difference = (97 - (numberLetter - (offset) % 26));
+                    output += String.fromCharCode(123 - difference);
+                }
+                else output += String.fromCharCode(numberLetter - offset % 26);
+            }
+            }
+        return output;
+}
 };
--- src/index.js
+++ src/index.js
@@ -1,45 +1,22 @@
-const normalText = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const specialText = " 0123456789¿?!¡,;:'.áéíóúñÑ";
-const filtro = normalText + specialText;
-let offset;
 
-document.getElementById("buttonStart").addEventListener("click", () => {
-    text = document.getElementById("textOne").value;
-    let offset = document.getElementById("offset").value;
-    const opcion = document.getElementById("list");
+document.getElementById('buttonStart').addEventListener('click', () => {
+    let encodeText = document.getElementById('textOne').value;
+    let offset = parseInt(document.getElementById('offset').value);
+    const opcion = document.getElementById('list');
     let value = opcion.options[opcion.selectedIndex].value;
-    if (value == "value1") {
-        document.getElementById("textTwo").innerHTML = cipher.decode(offset, text);
-        let result = document.getElementById("result")
-        result.innerHTML = "Texto desencriptado";
+    if (value == 'value1') {
+        let result = document.getElementById('result')
+        result.innerHTML = 'Texto desencriptado';
+        document.getElementById('textTwo').value = cipher.decode(offset, encodeText);
     }
     else {
-        let result = document.getElementById("result")
-        result.innerHTML = "Texto encriptado";
-        document.getElementById("textTwo").value = cipher.encode(offset, text);
+        let result = document.getElementById('result')
+        result.innerHTML = 'Texto encriptado';
+        document.getElementById('textTwo').value = cipher.encode(offset, encodeText);
     }
 })
-document.getElementById("clear").addEventListener("click", () => {
-    document.getElementById("textOne").value = "";
-    document.getElementById("textTwo").value = "";
+document.getElementById('clear').addEventListener('click', () => {
+    document.getElementById('textOne').value = '';
+    document.getElementById('textTwo').value = '';
 })
 
-document.getElementById("textOne").addEventListener("keyup", () => {
-    let out = '';
-    let string = document.getElementById("textOne").value;
-    for (let i = 0; i < string.length; i++)
-        if (filtro.includes(string.charAt(i)))
-            out += string.charAt(i);
-    document.getElementById("textOne").value = out;
-
-})
-
-document.getElementById("textTwo").addEventListener("keyup", () => {
-    let out = '';
-    let string = document.getElementById("textTwo").value;
-    for (i = 0; i < string.length; i++)
-        if (filtro.includes(string.charAt(i)))
-            out += string.charAt(i);
-    document.getElementById("textTwo").value = out;
-
-})
